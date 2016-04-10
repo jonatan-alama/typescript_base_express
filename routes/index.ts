0f696cc5a9f50667bf8dd650d119599ffb4e51d5
Jonatan Alama <jonatan@tinMac.local>
2016-04-10 19:32:20 +0900

    Revert "First commit"
    This reverts commit c7e8294c0079b364e5bb957fda2e18905ebf4088.

--- routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express = require("express");
-
-let router = express.Router();
-
-router.get("/", function (req: express.Request, res: express.Response, next: express.NextFunction) {
-    res.render("index", { title: "Typescript Base Express" });
-});
-
-export = router;
